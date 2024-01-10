f8e1c4fe73adadcf2cb9c5edd525d658542ed8a6
OIAZhuraev <OIAZhuraev@beelab.uz>
2024-01-10 07:20:52 +0500

    fixed cors header

--- src/tour/settings.py
+++ src/tour/settings.py
@@ -12,9 +12,9 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
@@ -26,12 +26,9 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['16.171.8.250', '127.0.0.1']
-CORS_ALLOWED_ORIGINS = [
-    "http://*",
-    "https://*",
-]
-
+# ALLOWED_HOSTS = ['16.171.8.250', '127.0.0.1']
+ALLOWED_HOSTS = ['*']
+# CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -59,6 +56,8 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+CORS_ORIGIN_ALLOW_ALL = True
+CORS_ALLOW_HEADERS = ['*']
 
 ROOT_URLCONF = 'tour.urls'
 AUTH_USER_MODEL = "user.User"
@@ -81,7 +80,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'tour.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
@@ -95,7 +93,6 @@ DATABASES = {
         "PORT": os.getenv("DB_PORT", "")
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
@@ -115,7 +112,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/5.0/topics/i18n/
 
@@ -126,7 +122,6 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
